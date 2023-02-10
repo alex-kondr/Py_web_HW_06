6d3cb0408d13b1b0d134d2b5c8ba84157cf51658
alex_kondr <alex_kondr@outlook.com>
2023-02-10 17:34:18 +0200

    save

--- query/query_12.sql
+++ query/query_12.sql
@@ -1,7 +1,18 @@
-SELECT e.Rating Rating, e.Date, stud.Name Student, g.Name Group_, sub.Name Subject
+SELECT e.Rating Rating, e.Date, stud.Name Student, stud.id, g.Name Group_, sub.Name Subject
 FROM Evaluations e
 JOIN Subjects sub ON sub.id = e.Subject_id
 JOIN Students stud ON stud.id = e.Student_id
 JOIN Groups g ON g.id = stud.Group_id
-WHERE sub.Name = 'art' AND g.Name = 'Adams-Jones'
-WHERE e.Date = (SELECT e2.Date FROM Evaluations e2 ORDER BY DESC LIMIT 1)
+WHERE sub.Name = 'art' 
+	AND g.Name = 'Adams-Jones'
+	AND e.Date = (	
+		SELECT e2.Date 
+		FROM Evaluations e2
+		JOIN Subjects sub ON sub.id = e2.Subject_id
+		JOIN Students stud ON stud.id = e2.Student_id
+		JOIN Groups g ON g.id = stud.Group_id 
+		WHERE sub.Name = 'art'
+			AND g.Name = 'Adams-Jones'
+		ORDER BY e2.Date DESC 
+		LIMIT 1
+	)
